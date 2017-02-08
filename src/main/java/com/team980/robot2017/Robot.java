28e4d20c93feb2a371071d7f1691171241da213e
Luke Myers <19lmyers@gmail.com>
2017-02-07 20:40:01 -0800

    Add project :)

---
+++ src/main/java/com/team980/robot2017/Robot.java
@@ -0,0 +1,36 @@
+package com.team980.robot2017;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+
+public class Robot extends IterativeRobot {
+
+    @Override
+    public void robotInit() {
+
+    }
+
+    @Override
+    public void autonomousInit() {
+
+    }
+
+    @Override
+    public void autonomousPeriodic() {
+
+    }
+
+    @Override
+    public void teleopInit() {
+
+    }
+
+    @Override
+    public void teleopPeriodic() {
+
+    }
+
+    @Override
+    public void disabledInit() {
+
+    }
+}
