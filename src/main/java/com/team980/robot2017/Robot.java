b0781898cf274d7f69ac4cc880011e55bc5da110
Luke Myers <19lmyers@gmail.com>
2017-02-16 19:45:23 -0800

    Add selectable autonomous to SmartDashboard

--- src/main/java/com/team980/robot2017/Robot.java
+++ src/main/java/com/team980/robot2017/Robot.java
@@ -2,6 +2,10 @@ package com.team980.robot2017;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.Scheduler;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends IterativeRobot {
 
@@ -9,22 +13,36 @@ public class Robot extends IterativeRobot {
     private Joystick driveWheel;
     private CustomDrive drive;
 
+    private SendableChooser<Command> autoChooser;
+
     @Override
     public void robotInit() {
         drive = new CustomDrive();
 
         driveStick = new Joystick(Parameters.DRIVE_JOYSTICK_ID);
         driveWheel = new Joystick(Parameters.DRIVE_WHEEL_ID);
+
+        autoChooser = new SendableChooser<>();
+        autoChooser.addObject("None", null);
+        SmartDashboard.putData("Autonomous mode chooser", autoChooser);
     }
 
     @Override
     public void autonomousInit() {
         drive.setHighGear(false);
+        Command autoCommand = autoChooser.getSelected();
+        if (autoCommand !=  null) {
+            System.out.println("Starting autonomous " + autoCommand.getName());
+            autoCommand.start();
+        } else {
+            System.out.println("No autonomous selected, idling");
+        }
     }
 
     @Override
     public void autonomousPeriodic() {
-
+        //REQUIRED to run scheduled auto command
+        Scheduler.getInstance().run();
     }
 
     @Override
